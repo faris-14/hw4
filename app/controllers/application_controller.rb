e362e2d57056941bd2f1e25215b7c1b26cd20db6
Alec NEwman <alecnwmn904@gmail.com>
2016-12-04 23:10:20 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def self.tweets
+      client.user_timeline('BBCNews')
+  end
+
+  def self.client
+      @client ||= Twitter::REST::Client.new do |config|
+          config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+          config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      end
+  end
+end
